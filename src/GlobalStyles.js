7a13d25ee23187f0635a09944cf9879641b0a0a9
Matheus Henrique Santos Da Silva <=>
2021-01-27 17:01:20 -0300

    Inicio Projeto Senai Overflow Web

---
+++ src/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+ * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: 'border-box';
+    }
+
+body {
+        font-family: 'Franklin Gothic Medium', 'Arial Marrow', Arial, Helvetica, sans-serif;
+    }
+
+
+`;
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+ width: 100vw;
+ height: 100vh;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ 
+
+`;
+
+export const FormLogin = styled.form`
+ width: 30%;
+ min-width: 300px;
+ display: flex;
+ flex-direction: colum;
+ align-items: center;
+
+`;
